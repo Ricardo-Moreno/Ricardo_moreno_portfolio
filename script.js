544cdff7aecda85f9625ad12c5deee129c1b36fc
Ricardo Moreno <ricardo.yvl@gmail.com>
2023-05-03 16:39:38 -0300

    espacio en la seccion sobre mi

--- script.js
+++ script.js
@@ -1,82 +1,58 @@
-// Seleccionar el header
-// const header = document.querySelector("header");
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener el enlace "Inicio"
+  const inicioLink = document.querySelector("#inicio-link");
 
-// // Agregar un event listener al hacer scroll
-// window.addEventListener("scroll", () => {
-//   // Comprobar si la posición de la ventana es mayor que la altura del header
-//   if (window.scrollY > header.offsetHeight) {
-//     // Agregar la clase "scroll-header" al header
-//     header.classList.add("scroll-header");
-//   } else {
-//     // Eliminar la clase "scroll-header" del header
-//     header.classList.remove("scroll-header");
-//   }
-// });
+  // Agregar un controlador de eventos al enlace
+  inicioLink.addEventListener("click", function () {
+    // Obtener el header
+    const header = document.querySelector("header");
 
-document.addEventListener("scroll", function () {
-  var scrollPosition = window.scrollY;
+    // Añadir la clase "scroll-header" para contraer el header
+    header.classList.add("scroll-header");
 
-  if (scrollPosition > 100) {
-    document.querySelector("header").classList.add("scroll-header");
+    // Obtener los elementos con la clase "header_2"
     const titleNames = document.getElementsByClassName("header_2");
+
+    // Añadir y eliminar las clases necesarias
     for (let i = 0; i < titleNames.length; i++) {
       titleNames[i].classList.add("titleNameNone");
       titleNames[i].classList.remove("header_2");
     }
-    const ulNav = document.getElementsByClassName("ul_nav");
-    console.log(ulNav);
-    for (let i = 0; i < ulNav.length; i++) {
-      ulNav[i].classList.add("ul_li_scroll");
-      ulNav[i].classList.remove("ul_nav");
-    }
+
+    // Obtener la barra de navegación
     const nav = document.querySelector("nav");
+
+    // Añadir y eliminar las clases necesarias
     nav.classList.add("nav_scroll");
     nav.classList.remove("nav");
-  } else {
-    document.querySelector("header").classList.remove("scroll-header");
-    const titleNames = document.getElementsByClassName("titleNameNone");
-    for (let i = 0; i < titleNames.length; i++) {
-      titleNames[i].classList.add("header_2");
-      titleNames[i].classList.remove("titleNameNone");
+  });
+
+  document.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+      document.querySelector("header").classList.add("scroll-header");
+      const titleNames = document.getElementsByClassName("header_2");
+      for (let i = 0; i < titleNames.length; i++) {
+        titleNames[i].classList.add("titleNameNone");
+        titleNames[i].classList.remove("header_2");
+      }
+      const nav = document.querySelector("nav");
+      nav.classList.add("nav_scroll");
+      nav.classList.remove("nav");
+    } else {
+      document.querySelector("header").classList.remove("scroll-header");
+      const titleNames = document.getElementsByClassName("titleNameNone");
+      for (let i = 0; i < titleNames.length; i++) {
+        titleNames[i].classList.add("header_2");
+        titleNames[i].classList.remove("titleNameNone");
+      }
+      const nav = document.querySelector("nav");
+      nav.classList.add("nav_scroll");
+      nav.classList.remove("nav");
     }
-    const ulNav = document.getElementsByClassName("ul_nav");
-    for (let i = 0; i < ulNav.length; i++) {
-      ulNav[i].classList.add("ul_nav");
-      ulNav[i].classList.remove("ul_li_scroll");
-    }
-    const nav = document.querySelector("nav");
-    nav.classList.add("nav_scroll");
-    nav.classList.remove("nav");
-  }
+  });
 });
-
-// document.addEventListener("scroll", function () {
-//   var scrollPosition = window.scrollY;
-
-//   if (scrollPosition > 100) {
-//     document.querySelector("header").classList.add("scroll-header");
-//     const titleName = document.getElementsByClassName("header_2");
-//     titleName.classList.add("titleNameNone");
-//     titleName.classList.remove("header_2");
-// const h2 = document.querySelector("h2");
-// h2.classList.add("h2-scroll");
-// h2.classList.remove("h2");
-// const ul = document.querySelector("ul");
-// ul.classList.add("ul_li_scroll");
-// ul.classList.remove("ul");
-// } else {
-//   document.querySelector("header").classList.remove("scroll-header");
-//   const titleName = document.getElementsByClassName("header_2");
-//   titleName.classList.add("header_2");
-//   titleName.classList.remove("titleNameNone");
-// const h2 = document.querySelector("h2");
-// h2.classList.add("h2");
-// h2.classList.remove("h2-scroll");
-// const ul = document.querySelector("ul");
-// ul.classList.add("ul");
-// ul.classList.remove("ul_li_scroll");
-//   }
-// });
 
 const links = document.querySelectorAll(".proyectos a,.a_card");
 
